65f96a0a620965331b2b203300e6ab70a871096b
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-08-07 15:11:45 +0300

    rename project

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { theme } from './theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename='/goit-react-hw-05-movies'>
+      <BrowserRouter basename="/Movies-react">
         <App />
       </BrowserRouter>
-      </ThemeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- Копіювати (1) src/components/Cast/Cast.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-
-export const CastTitle = styled.h2`
-color: green;
-margin-bottom: 24px;
-`
